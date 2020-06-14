a41f91b6788871419bab1574f343e5e91d728f91
Kara6000 <aqwkarahann@gmail.com>
2020-06-14 15:13:42 +0300

    updating our app with a shop page, collection preview and item component added

--- src/App.js
+++ src/App.js
@@ -3,19 +3,15 @@ import {Switch, Route} from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
 
 function App() {
   return (
     <div>
       <Switch> //stops loading others after the first match with switch and no exacts, /hats url would only render HomePage}
         <Route exact path='/' component={HomePage}/>
-        <Route exact path='/shop/hats' component={HatsPage}/>
+        <Route exact path='/shop' component={ShopPage}/>
       </Switch>
     </div>
   );
