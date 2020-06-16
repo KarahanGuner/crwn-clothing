1d122d80645d938e05469c60b375eef0853bc11d
Kara6000 <aqwkarahann@gmail.com>
2020-06-16 19:07:07 +0300

    implement firebase database and email autherization

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Header from './components/header/header.component.jsx';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 
 class App extends React.Component {
@@ -21,8 +21,22 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => { // when we mount starts a listener and looks for changes in user and sets the state to changed user. when we equal auth.onAuthStateChanged to a variable and summon it later it stops listening.
-      this.setState({currentUser: user});
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => { // when we mount starts a listener and looks for changes in user and sets the state to changed user. when we equal auth.onAuthStateChanged to a variable and summon it later it stops listening.
+      if(userAuth){
+        const userRef = await createUserProfileDocument(userAuth);
+        //like .get but installs a listener
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+        });
+        
+      } else {
+        this.setState({currentUser: userAuth});
+      }
     });
   }
   
