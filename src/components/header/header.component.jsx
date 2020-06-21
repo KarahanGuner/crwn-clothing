561698cdeb4f970812eca7778655cf1d81b6df62
Kara6000 <aqwkarahann@gmail.com>
2020-06-21 16:10:00 +0300

    added cartIcon and cartDropdown components to our application

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {auth} from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { ReactComponent as Logo} from '../../assets/crown.svg';
 import './header.styles.scss';
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser, hidden}) => (
     <div className='header'>
         <Link className='logo-container' to='/'>
            <Logo className='logo'/>
@@ -23,13 +25,20 @@ const Header = ({currentUser}) => (
                 :
                 <Link className='option' to='/signin'>SIGN IN</Link>
             }
+            <CartIcon />
         </div>
+        {
+            hidden ? null : (<CartDropdown/>) 
+        }
+        
     </div>
 );
 
 const mapStateToProps = state => ({ //function that allows us to access the state.
     //currentUser on the LHS is the one in line 8. state is rootreducer. state.user is the user reducer. and the currentUser in the RHS is the prop in user reducer .
-    currentUser: state.user.currentUser
+    currentUser: state.user.currentUser,
+    hidden: state.cart.hidden
+
 })
 
 export default connect(mapStateToProps)(Header); // connect may take two parameters and in return gives us another higher order function which then when we pass the Header component into we get state from the store.
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,7 +1,9 @@
 //root reducer tüm "store slice" oluşturan reducer'ların toplandığı yer
 import {combineReducers} from 'redux';
 import userReducer from './user/user.reducer';
+import cartReducer from './cart/cart.reducer';
 
 export default combineReducers({
-    user: userReducer
+    user: userReducer,
+    cart: cartReducer
 });
