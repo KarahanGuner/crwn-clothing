7bbb7d120492f6360aee2a395fad621058b4757f
Kara6000 <aqwkarahann@gmail.com>
2020-06-22 13:43:52 +0300

    updated cart utils to group same items

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,4 +1,5 @@
 //actions are functions that return objects. this is going to be dispatched
+//actionlar reducer'a geçirilecek objeleri return eden functionlar
 import {UserActionTypes} from './user.types';
 export const setCurrentUser = user => ({
     type: UserActionTypes.SET_CURRENT_USER,
