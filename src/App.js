805ad42cd728f527e31cc619d4396f7ba212e069
Kara6000 <aqwkarahann@gmail.com>
2020-07-13 18:12:59 +0300

    updated our app to use hooks in App Signin and Signup components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Switch, Route, Redirect} from 'react-router-dom';
 import {connect } from 'react-redux';
 
@@ -14,36 +14,27 @@ import {selectCurrentUser} from './redux/user/user.selectors';
 import {checkUserSession} from './redux/user/user.actions';
 
 
-class App extends React.Component {
-  unsubscribeFromAuth = null;
+const App = ({ checkUserSession, currentUser }) => {
+  useEffect(() => {
+    checkUserSession()
+  }, [checkUserSession]);
 
-  componentDidMount(){
-    const { checkUserSession } = this.props;
-    checkUserSession();
-  }
-  
-  componentWillUnmount(){
-    this.unsubscribeFromAuth();//stops listening when we close the component
-  }
-  render (){
-    return (
-      <div>
-        <Header />
-        <Switch> 
-          <Route exact path='/' component={HomePage}/>
-          <Route path='/shop' component={ShopPage}/>
-          <Route exact path='/checkout' component={CheckoutPage}/>
-          <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage/>)}/>
-        </Switch>
-      </div>
-    );
-  }
-  
-}
+  return (
+    <div>
+      <Header />
+      <Switch> 
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/shop' component={ShopPage}/>
+        <Route exact path='/checkout' component={CheckoutPage}/>
+        <Route exact path='/signin' render={() => currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage/>)}/>
+      </Switch>
+    </div>
+  );
+};
 
 const mapStateToProps = state => ({
   currentUser: selectCurrentUser(state)
-})
+});
 
 const mapDispatchToProps = dispatch => ({
   checkUserSession: () => dispatch(checkUserSession())
