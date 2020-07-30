1d969557d9050e4f4bb702de1dcf71a8fbafa0f8
Kara6000 <aqwkarahann@gmail.com>
2020-07-30 16:51:04 +0300

    adding compression library and optimizations

--- client/src/components/with-spinner/with-spinner.component.jsx
+++ client/src/components/with-spinner/with-spinner.component.jsx
@@ -1,19 +1,10 @@
 import React from 'react';
 //this is a higher order component. it takes a component and wraps it.
 
-import {SpinnerContainer, SpinnerOverlay} from './with-spinner.styles';
+import Spinner from '../spinner/spinner.component';
 
-const WithSpinner = WrappedComponent => {
-    const Spinner = ({isLoading, ...otherProps}) => {
-        return isLoading ? (
-            <SpinnerOverlay>
-                <SpinnerContainer />
-            </SpinnerOverlay>
-        ) : (
-            <WrappedComponent {...otherProps}/> 
-            )
-    };
-    return Spinner;
+const WithSpinner = WrappedComponent => ({isLoading, ...otherProps}) => {
+    return isLoading ? <Spinner/> : <WrappedComponent {...otherProps}/> ;
 };
 
 export default WithSpinner;
