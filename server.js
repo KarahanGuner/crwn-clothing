2c304a89038bd2f91dcc8bbccda53ddc5e892fec
Kara6000 <aqwkarahann@gmail.com>
2020-08-01 22:46:25 +0300

    making our website a pwa, also added serviceworker

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 const compression = require('compression');
+const enforce = require('express-sslify');
 
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
@@ -16,6 +17,7 @@ const port = process.env.PORT || 5000;
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(enforce.HTTPS({trustProtoHeader: true}));
 app.use(cors()); //it allows us to make requests from 3000 to 5000.
 
 if(process.env.NODE_ENV === 'production'){
@@ -25,6 +27,10 @@ if(process.env.NODE_ENV === 'production'){
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     })
 }
+
+app.get('/service-worker.js', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
+});
 
 app.post('/payment', (req, res) => {
     const body = {
@@ -40,7 +46,7 @@ app.post('/payment', (req, res) => {
             res.status(200).send({success: stripeRes});
         }
     });
-})
+});
 
 
 
