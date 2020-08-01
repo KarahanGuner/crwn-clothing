5af41f6f555eff6b98120829dd4e8d7268b510a2
Kara6000 <aqwkarahann@gmail.com>
2020-08-01 22:57:29 +0300

    putting compression and enforce.HTTPS to only production

--- server.js
+++ server.js
@@ -14,13 +14,14 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 
-app.use(compression());
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(enforce.HTTPS({trustProtoHeader: true}));
 app.use(cors()); //it allows us to make requests from 3000 to 5000.
 
 if(process.env.NODE_ENV === 'production'){
+    app.use(compression());
+    app.use(enforce.HTTPS({trustProtoHeader: true}));
     app.use(express.static(path.join(__dirname, 'client/build')));
 
     app.get('*', function(req,res){
